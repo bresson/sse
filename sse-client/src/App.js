078b71bf1e2ea0d3294e1e4d1bea5b915179034f
bresson <bressonnemesis@gmail.com>
2020-04-07 21:54:37 -0400

    #comment now with composed usefeefects

--- sse-client/src/App.js
+++ sse-client/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import * as Ramda from "ramda";
 
-function App() {
+const useSSE = eventsource => {
   const [nests, setNests] = useState([]);
   const [listening, setListening] = useState(false);
 
   useEffect(() => {
     if (!listening) {
-      const events = new EventSource("http://localhost:3000/events");
+      const events = new EventSource(eventsource);
       events.onmessage = (event) => {
         const parsedData = JSON.parse(event.data);
 
@@ -16,28 +17,66 @@ function App() {
 
       setListening(true);
     }
-  }, [listening, nests]);
+  }, [listening, nests, eventsource]);
 
-  return (
-    <table className="stats-table">
-      <thead>
-        <tr>
-          <th>Momma</th>
-          <th>Eggs</th>
-          <th>Temperature</th>
-        </tr>
-      </thead>
-      <tbody>
-        {nests.map((nest, i) => (
-          <tr key={i}>
-            <td>{nest.momma}</td>
-            <td>{nest.eggs}</td>
-            <td>{nest.temperature} ℃</td>
+  return nests;
+}
+
+function App() {
+  // const [nests, setNests] = useState([]);
+  // const [listening, setListening] = useState(false);
+
+  // useEffect(() => {
+  //   if (!listening) {
+  //     const events = new EventSource("http://localhost:3000/events");
+  //     events.onmessage = (event) => {
+  //       const parsedData = JSON.parse(event.data);
+
+  //       setNests((nests) => nests.concat(parsedData));
+  //     };
+
+  //     setListening(true);
+  //   }
+  // }, [listening, nests]);
+
+  const [dat, setDat] = useState([]);
+
+  const nests = useSSE("http://localhost:3000/events");
+
+  useEffect(() => {
+    if (nests.length) {
+      debugger
+      const _newDat = Ramda.union(dat, nests)
+      setDat(_newDat)
+    }
+  }, [nests])
+
+  if (dat.length) {
+    return (
+      <table className="stats-table">
+        <thead>
+          <tr>
+            <th>Momma</th>
+            <th>Eggs</th>
+            <th>Temperature</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+        </thead>
+        <tbody>
+          {nests.map((nest, i) => (
+            <tr key={i}>
+              <td>{nest.momma}</td>
+              <td>{nest.eggs}</td>
+              <td>{nest.temperature} ℃</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  } else {
+    return <h1>Loading</h1>;
+  }
+
+
 }
 
 export default App;
