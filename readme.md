8fb23513ec09de99c8bd169b6b7b3ee0e3823444
JB <jesse.polyglot@gmail.com>
2020-05-08 19:30:19 -0400

    ?

---
+++ readme.md
@@ -0,0 +1,5 @@
+# File upload with Multer, Axios
+* Both /upload & /upload2 work
+  * upload -> /uploads dir
+  * upload2 -> /uploads dir
+It also looks like an earlier attempt works and deposited to the same root as express-server! Which version? 
--- sse-client/src/uploads.js
+++ sse-client/src/uploads.js
@@ -1,54 +1,62 @@
-import React, { useState, useEffect } from "react"
-import { Form, Button, TextInput, FormField } from "grommet"
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import { Form, Button, TextInput, FormField } from "grommet";
+import axios from "axios";
 
-const Upload = props => {
-    const [file, setFile] = useState()
+const Upload = (props) => {
+  const [file, setFile] = useState();
 
-    const handleFile = file => {
-        console.log("hendle file ", file)
-        setFile(file)
+  const handleFile = (file) => {
+    console.log("hendle file ", file);
+    setFile(file);
+  };
+
+  const handleSubmit = (vals) => {
+    console.log("submit ", vals);
+    // console.log("icon ", file, file[0])
+    const fdata = new FormData();
+    for (let key in vals) {
+      console.log(key.toString());
+      fdata.append(key.toString(), vals[key]);
     }
-
-    const handleSubmit = vals => {
-        console.log("submit ", vals)
-        // console.log("icon ", file, file[0])
-        const fdata = new FormData()
-        for (let key in vals) {
-            console.log(key.toString())
-            fdata.append(key.toString(), vals[key])
-        }
-        // fdata.append("Icon", file)
-        for (var value of fdata.values()) {
-            console.log(value);
-        }
-        axios({
-            method: "post",
-            url: "http://localhost:5000/upload2",
-            data: fdata,
-            headers: {
-                'Content-Type': `multipart/form-data`,
-            }
-        })
+    // fdata.append("Icon", file)
+    for (var value of fdata.values()) {
+      console.log(value);
     }
-    return (
-        // encType="multipart/form-data" 
-        <Form onSubmit={({ value }) => {
-            console.log(value)
-            return handleSubmit(Object.assign({}, value, { Icon: file }))
-            // return handleSubmit(value)
-        }} >
-            <FormField name="name" htmlfor="text-input-id" label="Name">
-                <TextInput id="text-input-id" name="name" required />
-            </FormField>
-            <FormField name="Icon" label="file" onChange={(e) => handleFile(e.target.files[0])}>
-                <TextInput type="file" name="Icon" required />
-            </FormField>
-            {/* <input type="file" name="Icon" onChange={(e) => handleFile(e.target.files[0])} /> */}
+    axios({
+      method: "post",
+      url: "http://localhost:5000/upload",
+      data: fdata,
+      headers: {
+        "Content-Type": `multipart/form-data`,
+      },
+    });
+  };
+  return (
+    // encType="multipart/form-data"
+    <Form
+      onSubmit={({ value }) => {
+        console.log(value);
+        return handleSubmit(Object.assign({}, value, { Icon: file }));
+        // return handleSubmit(value)
+      }}
+    >
+      <FormField name="name" htmlfor="text-input-id" label="Name">
+        <TextInput id="text-input-id" name="name" required />
+      </FormField>
+      <FormField
+        name="Icon"
+        label="file"
+        onChange={(e) => handleFile(e.target.files[0])}
+      >
+        <TextInput type="file" name="Icon" required />
+      </FormField>
+      {/* <input type="file" name="Icon" onChange={(e) => handleFile(e.target.files[0])} /> */}
 
-            <Button primary type="submit">Submit</Button>
-        </Form>
-    )
-}
+      <Button primary type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
 
 export default Upload;
